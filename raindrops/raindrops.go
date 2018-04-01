3e3da5955b8b6a41fe613b8c53a480023ea976b9
Mixichao <mixichao@jd.com>
2018-04-01 12:02:51 +0800

    [Update] 添加练习7的最佳实现

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -20,3 +20,31 @@ func Convert(number int) string {
 	}
 	return strconv.Itoa(number)
 }
+
+/*
+package raindrops
+
+import "strconv"
+
+var factors = [3]struct {
+	Value int
+	Word  string
+}{
+	{3, "Pling"},
+	{5, "Plang"},
+	{7, "Plong"},
+}
+
+func Convert(input int) string {
+	response := ""
+	for _, factor := range factors {
+		if input%factor.Value == 0 {
+			response += factor.Word
+		}
+	}
+	if len(response) == 0 {
+		response += strconv.Itoa(input)
+	}
+	return response
+}
+*/
