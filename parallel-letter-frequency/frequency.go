16bfc794bed5fc35439de003ab371f2928b3a44e
Mixichao <mixichao@jd.com>
2018-04-04 17:18:34 +0800

    [Update] 添加练习15最佳实现

--- parallel-letter-frequency/frequency.go
+++ parallel-letter-frequency/frequency.go
@@ -17,19 +17,80 @@ func Frequency(s string) FreqMap {
 func ConcurrentFrequency(strList []string) FreqMap {
 	m := FreqMap{}
 	a := new(sync.Mutex)
-	forever := make(chan bool)
+	wg := new(sync.WaitGroup)
 	for _, str := range strList {
+		wg.Add(1)
 		go func(input string) {
 			for _, r := range input {
 				a.Lock()
 				m[r]++
 				a.Unlock()
 			}
-			forever <- true
+			wg.Done()
 		}(str)
 	}
-	<-forever
-	<-forever
-	<-forever
+	wg.Wait()
 	return m
 }
+
+/*最佳实现1
+func ConcurrentFrequency(s []string) FreqMap {
+	var wg sync.WaitGroup
+	counters := make([]FreqMap, len(s))
+
+	for i, sentence := range s {
+		wg.Add(1)
+		go func(counters []FreqMap, i int, sentence string) {
+			counters[i] = Frequency(sentence)
+			wg.Done()
+		}(counters, i, sentence)
+	}
+
+	wg.Wait()
+
+	result := FreqMap{}
+	for _, counter := range counters {
+		for key, value := range counter {
+			result[key] += value
+		}
+	}
+
+	return result
+}
+*/
+/*最佳实现2
+func ConcurrentFrequency(ss []string) FreqMap {
+	fm := FreqMap{}
+	cc := make(chan FreqMap)
+
+	for _, s := range ss {
+		go func(s string) {
+			cc <- Frequency(s)
+		}(s)
+	}
+
+	for i := 0; i < len(ss); i++ {
+		fm.merge(<-cc)
+	}
+
+	return fm
+}
+
+func (f FreqMap) merge(f2 FreqMap) FreqMap {
+	for k, v := range f2 {
+		lock.RLock()
+		_, ok := f[k]
+		lock.RUnlock()
+
+		lock.Lock()
+		if !ok {
+			f[k] = 0
+		}
+		f[k] += v
+		lock.Unlock()
+
+	}
+
+	return f
+}
+*/
